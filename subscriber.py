2ac00df1b26536a6ea1fb2c4966a0b4e7e971103
cowdingus <zaki.nadhif45@gmail.com>
2021-10-13 21:05:58 +0700

    Rewrite subscriber to have a context manager

--- subscriber.py
+++ subscriber.py
@@ -1,13 +1,22 @@
-from typing import Optional
 from scheduler import Scheduler, Event
 
+from typing import Optional
 from threading import Lock
+import functools
+
 
 class Subscriber:
-    def __init__(self):
+    def __enter__(self):
         self.event_queue = []
         self.subscription_ids = {}
         self.mutex = Lock()
+        return self
+
+    def __exit__(self, *exc):
+        if not self.subscription_ids:
+            return
+
+        self._unsubscribe_all()
 
     def poll_event(self) -> Optional[Event]:
         with self.mutex:
@@ -15,13 +24,24 @@ class Subscriber:
                 return self.event_queue.pop()
         return None
 
-    def add_event_to_queue(self, event: Event) -> None:
+    def set_default_scheduler(self, scheduler: Scheduler) -> None:
+        self.default_scheduler = scheduler
+
+    def subscribe_into(self, group: str, scheduler: Scheduler = None) -> None:
+        if scheduler is None:
+            assert self.default_scheduler, """
+            Default scheduler is not set, yet subscribe_into is called
+            without destination scheduler passed in.
+            """
+
+            scheduler = self.default_scheduler
+
+        id = scheduler.subscribe(group, self._add_event_to_queue)
+        self._append_into_subscription_list(id, scheduler)
+
+    def _add_event_to_queue(self, event: Event) -> None:
         with self.mutex:
             self.event_queue.append(event)
-
-    def subscribe_into(self, group: str, scheduler: Scheduler) -> None:
-        id = scheduler.subscribe(group, self.add_event_to_queue)
-        self._append_into_subscription_list(id, scheduler)
 
     def _append_into_subscription_list(self, id: int, scheduler: Scheduler) -> None:
         if self.subscription_ids.get(scheduler) is None:
@@ -34,8 +54,21 @@ class Subscriber:
             for id in ids:
                 scheduler.unsubscribe(id)
 
-    def __del__(self):
-        if not self.subscription_ids:
-            return
+        self.subscription_ids.clear()
 
-        self._unsubscribe_all()
+
+_USE_SUBSCRIBER_KEY = "_subscriber"
+
+
+def use_subscriber(func):
+    @functools.wraps(func)
+    def wrapper_use_subscriber(*args, **kwargs):
+        with Subscriber() as subscriber:
+            kwargs[_USE_SUBSCRIBER_KEY] = subscriber
+            return func(*args, **kwargs)
+
+    return wrapper_use_subscriber
+
+
+def get_subscriber(kwargs: dict) -> Subscriber:
+    return kwargs[_USE_SUBSCRIBER_KEY]
