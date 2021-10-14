2c30f2f702ee3245d4962e971e321d48c6a65ae7
cowdingus <zaki.nadhif45@gmail.com>
2021-10-14 07:10:54 +0700

    Remodel `printer.py` to use subscriber.wait_event

--- printer.py
+++ printer.py
@@ -1,6 +1,8 @@
-from subscriber import Subscriber, use_subscriber, get_subscriber
+import logging
+from subscriber import use_subscriber, get_subscriber
 from scheduler import Scheduler
 import time
+
 
 @use_subscriber
 def printer_task(scheduler: Scheduler, **kwargs):
@@ -10,9 +12,7 @@ def printer_task(scheduler: Scheduler, **kwargs):
     task_subscriber.subscribe_into("printer")
     task_subscriber.subscribe_into("flow_control")
 
-    while True:
-        task = task_subscriber.poll_event()
-
+    while task := task_subscriber.wait_event():
         if task is None:
             time.sleep(0.2)
             continue
@@ -21,4 +21,5 @@ def printer_task(scheduler: Scheduler, **kwargs):
             print("Printer:", task.arguments[0])
 
         if task.name == "terminate":
+            logging.debug(f"Printer: Got a terminate event. Terminating.")
             break
